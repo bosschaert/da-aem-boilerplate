824a2667f8605f31947dd376b96817b5f4e698ab
David Bosschaert <bosschae@adobe.com>
2024-06-17 16:42:43 +0100

    wip

--- .github/workflows/node/schedule-unpublish.js
+++ .github/workflows/node/schedule-unpublish.js
@@ -1,5 +1,12 @@
+async function removeFromIdx() {
+  const idx = await fetch('https://main--jmp-da--jmphlx.aem.live/jmp-en.json');
+  console.log('Read', await idx.json());
+}
 
+// Remove non-arguments from argv
 process.argv.shift();
 process.argv.shift();
 
-console.log("Hello", process.argv);
+removeFromIdx().then(() => {
+  console.log('Process completed');
+});
