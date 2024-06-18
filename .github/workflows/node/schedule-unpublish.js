cd30f8dbf4389b370e93b4e5d4dab276036523b2
David Bosschaert <bosschae@adobe.com>
2024-06-18 10:32:13 +0100

    Cron action

--- .github/workflows/node/schedule-unpublish.js
+++ .github/workflows/node/schedule-unpublish.js
@@ -1,11 +1,28 @@
-async function removeFromIdx() {
-  const idx = await fetch('https://main--jmp-da--jmphlx.aem.live/jmp-en.json');
-  console.log('Read', await idx.json());
+async function checkDocuments(data) {
+  const now = new Date().getTime();
+  console.log('Now', now);
+  for (const doc of data) {
+    const offTime = new Date(Number(doc.offtime));
+    console.log('Found', doc.path, 'offTime', offTime);
+
+    if (offTime.getTime() < now) {
+      console.log(`Unpublishing ${doc.path}`);
+
+      const url = `https://admin.hlx.page/live/bosschaert/da-aem-boilerplate/main${doc.path}`;
+      const resp = await fetch(url, { method: 'DELETE' });
+      console.log('Unpublish response', resp.status);
+    }
+  }
 }
 
-// Remove non-arguments from argv
-process.argv.shift();
-process.argv.shift();
+async function removeFromIdx() {
+  const idx = await fetch('https://main--da-aem-boilerplate--bosschaert.hlx.live/jmp-all.json');
+  const idxJson = await idx.json();
+  const data = idxJson.data;
+  if (data) {
+    await checkDocuments(data);
+  }
+}
 
 removeFromIdx().then(() => {
   console.log('Process completed');
