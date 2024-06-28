dadca11beff1ccd500b372b699639770c43b8500
David Bosschaert <bosschae@adobe.com>
2024-06-28 12:02:03 +0100

    Select tags

--- tools/tags/tag-selector.js
+++ tools/tags/tag-selector.js
@@ -1,5 +1,4 @@
-// import { LitElement } from '/deps/lit/lit-core.min.js';
-import { LitElement, html } from 'https://da.live/deps/lit/lit-core.min.js';
+import { LitElement, html, until } from 'https://da.live/deps/lit/lit-all.min.js';
 
 export default class DaTagSelector extends LitElement {
   static properties = {
@@ -7,10 +6,55 @@ export default class DaTagSelector extends LitElement {
     token: { type: String },
   };
 
+  getTagURL() {
+    const host = this.project.ref === 'local'
+      ? 'http://localhost:3000'
+      : `https://${this.project.ref}--${this.project.repo}--${this.project.org}.hlx.live`;
+
+    const url = `${host}/data/producttags.json`;
+    return url;
+  }
+
+  async fetchTags() {
+    const url = this.getTagURL();
+    console.log('Fetching', url);
+    const resp = await fetch(url);
+    const tagData = await resp.json();
+
+    const categories = new Map();
+    tagData.data.forEach((el) => {
+      const k = Object.keys(el)[0];
+      const v = Object.values(el)[0];
+      let vals = categories.get(k);
+      if (!vals) {
+        vals = [];
+        categories.set(k, vals);
+      }
+      vals.push(v);
+    });
+
+    const tagLists = [];
+    categories.forEach((v, k) => {
+      const el = html`<h2>${k}</h2>
+      <ul>
+        ${v.map((tag) => html`<li>${tag}</li>`)}
+      </ul>`
+      tagLists.push(el);
+    });
+    return tagLists;
+  }
+
+  listTags() {
+    return html`<p>${until(this.fetchTags(), html`Fetching tags...</p>`)}`;
+  }
+
   render() {
-    return html`<h2>From the tag selector</h2>
-      <p>Project: ${JSON.stringify(this.project)}</p>
-      <p>Token: ${this.token}</p>
+    return html`
+      ${this.listTags()}
+      <br>
+      <small>List obtained from: ${this.getTagURL()}</small>
+      <br>
+      <small>Project: ${JSON.stringify(this.project)}</small>
     `;
   }
 }
